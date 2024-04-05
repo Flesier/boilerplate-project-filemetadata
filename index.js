be9ef4845fcc589a1a5226e46ba487ff04c3b717
Flesier <flesieronyango18@gmail.com>
2024-04-05 18:04:21 +0300

    Refactor file upload functionality in API endpoint

--- index.js
+++ index.js
@@ -1,34 +1,26 @@
 var express = require('express');
-var multer = require('multer');
-var upload = multer({ dest: 'uploads/' });
 var cors = require('cors');
-require('dotenv').config();
+const multer  = require('multer')
+const upload = multer({ dest: 'uploads/' })
+require('dotenv').config()
 
 var app = express();
 
-// Basic configuration
 app.use(cors());
-
 app.use('/public', express.static(process.cwd() + '/public'));
 
 app.get('/', function (req, res) {
     res.sendFile(process.cwd() + '/views/index.html');
 });
 
-// API endpoint
-app.post('/api/fileanalyse', upload.single('upfile'), (req, res) => {
-  const getName = req.file.originalname,
-  getType = req.file.mimetype,
-  getSize = req.file.size
-
-  return res.json({
-    name: getName,
-    type: getType,
-    size: getSize
-  });
+app.post('/api/fileanalyse', upload.single('upfile'), function (req, res) {
+  const fileName = req.file.originalname;
+  const fileType = req.file.mimetype;
+  const fileSize = req.file.size;
+  res.json({name: fileName, type: fileType, size: fileSize})
 });
 
 const port = process.env.PORT || 3000;
 app.listen(port, function () {
-  console.log('Your app is listening on port ' + port);
+  console.log('Your app is listening on port ' + port)
 });
